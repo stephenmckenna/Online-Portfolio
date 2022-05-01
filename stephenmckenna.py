d9a2f7b56573a69a0c0d64cc50b62273b3465495
Stephen McKenna <mckennask00@gmail.com>
2022-04-30 21:12:31 -0400

    About Me Page finished

--- stephenmckenna.py
+++ stephenmckenna.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def test():
-    return render_template("homeTemplate.html")
+def root():
+    return redirect(url_for('aboutMe'))
  
+@app.route("/about-me")
+def aboutMe():
+    return render_template("aboutMe.html")
+
+@app.route("/education")
+def education():
+    return render_template("education.html")
+
+@app.route("/experience")
+def experience():
+    return render_template("experience.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
