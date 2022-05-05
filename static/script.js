f7548033609919af40b62ec4f35fab335c99833e
Stephen McKenna <mckennask00@gmail.com>
2022-05-05 14:07:40 -0400

    experience page done

--- static/script.js
+++ static/script.js
@@ -1,34 +1,44 @@
 let curPage;
-// Add event listeners for page buttons
-    document.getElementById('aboutMeBtn').addEventListener('click', () => setPage());
-    document.getElementById('educationBtn').addEventListener('click', () => setPage());
-    document.getElementById('experienceBtn').addEventListener('click', () => setPage());
-    document.getElementById('projectsBtn').addEventListener('click', () => setPage());
 
 function setPage() {
-    window.addEventListener("DOMContentLoaded", function() {
     curPage = document.getElementById('title').innerHTML;
     console.log(curPage);
         if(curPage.localeCompare('Experience') == 0)
         {
             console.log('experience page');
-            const obs = new IntersectionObserver(entries => {
+            const divObs = new IntersectionObserver(entries => {
                 entries.forEach(entry => {
-                    console.log(entry);
                     if(entry.isIntersecting) {
                         entry.target.classList.add('divider-animation');
                     }
                 });
             });
-        
-            console.log(document.querySelectorAll(".divider"));
+            const itemObs = new IntersectionObserver(entries => {
+                entries.forEach(entry => {
+                    if(entry.isIntersecting) {
+                        entry.target.classList.add('item-animation');
+                    }
+                });
+            });
         
             const dividerList = document.querySelectorAll(".divider");
+            const itemList = document.querySelectorAll(".item");
         
             dividerList.forEach(divider => {
-                obs.observe(divider);
+                divObs.observe(divider);
             })
-        }
-    }, false);
-    
+            itemList.forEach(item => {
+                itemObs.observe(item);
+            })
+        }    
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+    // Add event listeners for page buttons
+    document.getElementById('aboutMeBtn').addEventListener('click', () => setPage());
+    document.getElementById('educationBtn').addEventListener('click', () => setPage());
+    document.getElementById('experienceBtn').addEventListener('click', () => setPage());
+    document.getElementById('projectsBtn').addEventListener('click', () => setPage());
+
+    setPage();
+});
