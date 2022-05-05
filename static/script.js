d37a649d39bd282aeb75b87542f33914f8c5435f
Stephen McKenna <mckennask00@gmail.com>
2022-05-05 12:22:15 -0400

    added changes, script.js still not working

--- static/script.js
+++ static/script.js
@@ -1,37 +1,34 @@
-let curPage = 'about'; //about page loaded on site open
-
+let curPage;
 // Add event listeners for page buttons
-    document.getElementById('aboutMeBtn').addEventListener('click', () => setPage('about'));
-    document.getElementById('educationBtn').addEventListener('click', () => setPage('education'));
-    document.getElementById('experienceBtn').addEventListener('click', () => setPage('experience'));
-    document.getElementById('projectsBtn').addEventListener('click', () => setPage('projects'));
+    document.getElementById('aboutMeBtn').addEventListener('click', () => setPage());
+    document.getElementById('educationBtn').addEventListener('click', () => setPage());
+    document.getElementById('experienceBtn').addEventListener('click', () => setPage());
+    document.getElementById('projectsBtn').addEventListener('click', () => setPage());
 
-function setPage(page) {
-    curPage = page;
+function setPage() {
+    window.addEventListener("DOMContentLoaded", function() {
+    curPage = document.getElementById('title').innerHTML;
+    console.log(curPage);
+        if(curPage.localeCompare('Experience') == 0)
+        {
+            console.log('experience page');
+            const obs = new IntersectionObserver(entries => {
+                entries.forEach(entry => {
+                    console.log(entry);
+                    if(entry.isIntersecting) {
+                        entry.target.classList.add('divider-animation');
+                    }
+                });
+            });
+        
+            console.log(document.querySelectorAll(".divider"));
+        
+            const dividerList = document.querySelectorAll(".divider");
+        
+            dividerList.forEach(divider => {
+                obs.observe(divider);
+            })
+        }
+    }, false);
     
-    if(curPage.localeCompare('experience') == 0)
-    {
-        window.onload = scrollAnimations()
-    }
 }
-
-function scrollAnimations() {
-    //Scroll triggered animations
-    const obs = new IntersectionObserver(entries => {
-        entries.foreach(entry => {
-            console.log(entry);
-            if(entry.isIntersecting) {
-                entry.target.classList.add('divider-animation');
-            }
-        });
-    });
-
-    console.log(document.querySelectorAll(".divider"));
-
-    const dividerList = document.querySelectorAll(".divider");
-
-    dividerList.foreach(divider => {
-        obs.observe(divider);
-    })
-}
-
