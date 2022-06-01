51226a266e1ad063465cf026d9ba7f255347c9c0
Stephen McKenna <mckennask00@gmail.com>
2022-05-31 20:24:32 -0400

    all but projects and animations done

--- static/script.js
+++ static/script.js
@@ -2,7 +2,6 @@ let curPage;
 
 function setPage() {
     curPage = document.getElementById('title').innerHTML;
-    console.log(curPage);
         if(curPage.localeCompare('Experience') == 0)
         {
             console.log('experience page');
@@ -31,6 +30,55 @@ function setPage() {
                 itemObs.observe(item);
             })
         }    
+
+        document.getElementById('contactMeBtn').addEventListener("click", () => {
+            document.getElementById('topWrapper').className='blurring';
+            document.getElementById('contactMeContainer').style.display='flex';
+            document.getElementById('overlay').style.display='block';
+
+            document.getElementById('closeBtn').addEventListener("click", () => closeForm());
+
+            //reloads on this click, which i dont want
+            document.getElementById('submitForm').addEventListener("click", () => {
+                sendMessage();
+                closeForm();
+            });
+        });
+        
+}
+
+function closeForm() {
+    console.log('closing form');
+    document.getElementById('topWrapper').classList.remove('blurring');
+    document.getElementById('contactMeContainer').style.display='none';
+    document.getElementById('overlay').style.display='none';
+    document.getElementById('topWrapper').classList='unblurring';
+    setTimeout(() => {document.getElementById('topWrapper').classList.remove('unblurring');}, 250);
+    
+}
+
+function sendMessage() {
+    const name = document.getElementById('nameInput').value;
+    const email = document.getElementById('emailInput').value;
+    const msg = document.getElementById('msgInput').value;
+    
+    fetch("/newMessage", {
+        method: "post",
+		headers: { "Content-type": "application/x-www-form-urlencoded; charset=UTF-8" },
+		body: `name=${name}&email=${email}&msg=${msg}`
+    })
+    .then((response) => {
+        clearInput();
+    })
+    .catch(() => {
+        console.log("Error sending message!");
+    });
+}
+
+function clearInput() {
+	document.getElementById("nameInput").value = "";
+	document.getElementById("emailInput").value = "";
+	document.getElementById("msgInput").value = "";
 }
 
 window.addEventListener("DOMContentLoaded", () => {
--- stephenmckenna.py
+++ stephenmckenna.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+import json
+import smtplib
 
 app = Flask(__name__)
 
@@ -21,3 +23,37 @@ def experience():
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
+
+@app.route("/newMessage", methods=['POST'])
+def newMessage():
+    name = request.form['name']
+    email = request.form['email']
+    msg = request.form['msg']
+    ret = ['OK', 204]
+
+    gmail_user = 'mckennask00@gmail.com'
+    
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, 'tbwidjlsdcrpnwtb')
+    except:
+        print('could not log into gmail')
+
+    sent_from = gmail_user
+    to = gmail_user
+    subject = 'Contact Me - {}'.format(name)
+    body = 'New contact from website!\n\n{}\n\n{} can be reached at {}'.format(msg,name,email)
+
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
+
+    %s
+    """ % (sent_from, to, subject, body)
+
+    server.sendmail(sent_from, to, email_text)
+    server.close()
+
+    return json.dumps(ret)
